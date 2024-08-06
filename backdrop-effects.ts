63d3351ba37e88c937f6e51041fbb29bbf381075
BrokenGamingGames <emersonejohnson@gmail.com>
2024-08-06 05:34:34 -0700

    Initial files for MakeCode project

---
+++ backdrop-effects.ts
@@ -0,0 +1,30 @@
+namespace effects{
+        //% fixedInstance whenUsed block="rain"
+        export const rain = new ScreenEffect(0, 750, 0, function (anchor: particles.ParticleAnchor, particlesPerSecond: number){
+            const factory = new particles.RainFactory(160);
+            const source = new particles.ParticleSource(anchor, particlesPerSecond, factory);
+            factory.setDirection(360, 0);
+            factory.setSpeed(200);
+            // render behind tile map
+            source.z = -4;
+            return source
+        });
+
+    //% fixedInstance whenUsed block="storm"
+    export const storm = new ScreenEffect(.5, 1.5, 5000, function (anchor: particles.ParticleAnchor, particlesPerSecond: number) {
+        const factory = new particles.StormFactory();
+        const source = new particles.ParticleSource(anchor, particlesPerSecond, factory);
+
+        // render behind tile map
+        source.z = -2;
+        return source;
+    });
+    //% fixedInstance whenUsed block="lightning"
+    export const lightning = new ScreenEffect(10, 1, 4000, function (anchor: particles.ParticleAnchor, particlesPerSecond: number) {
+        const factory = new particles.LightningFactory(anchor.width ? anchor.width : 16, 16);
+        const source = new particles.ParticleSource(anchor, particlesPerSecond, factory);
+        factory.setSpeed(30);
+        source.z = -3
+        return source;
+    });
+}
---
+++ main.ts
@@ -0,0 +1,4 @@
+tiles.setCurrentTilemap(tilemap`level2`)
+effects.storm.startScreenEffect()
+effects.lightning.startScreenEffect()
+effects.rain.startScreenEffect()
